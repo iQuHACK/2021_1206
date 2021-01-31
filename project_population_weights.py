850265ec229e86f1d586ba6fc290e091f44db5dc
eqham <epham@mit.edu>
2021-01-31 16:27:09 +0000

    clean

--- project_population_weights.py
+++ project_population_weights.py
@@ -9,7 +9,6 @@ from dwave.system import LeapHybridSampler
 from util import great_circle_distance
 import pylab
 import os
-import gmplot
 
 import matplotlib
 try:
@@ -21,117 +20,141 @@ except ImportError:
 
 def load_csv(filename, dtype=None, keep=None):
     '''
-    Returns a pandas df from a csv file. keep (list) contains columns to keep.
+    Read in location data from csv and return a pandas dataframe
+
+        PARAMS:
+            filename (str) - imported files
+            dtype (list) - column headers
+            keep (list) - keep these columns and discard remaining columns
     '''
+
     df = pd.read_csv(filename, header=0)
-    df['Latitude'] = df['Latitude'].replace("–","", regex=True).astype(float)
-    df['Longitude'] = df['Longitude'].replace("–","", regex=True).astype(float)
+    df['Latitude'] = df['Latitude'].replace("–", "", regex=True).astype(float)
+    df['Longitude'] = df['Longitude'].replace(
+        "–", "", regex=True).astype(float)
     if keep:
         df = df[keep]
+
     return df
+
 
 def load_data(distribution_center_file, population_file):
+    '''
+    Returns data from files as a pandas dataframe
 
+        PARAMS:
+            distribution_center_file (str) - maps latitude and longitude to distribution sites
+            population_file (str) - maps latitude and longitude to county
     '''
-    Returns data from files as pandas df
-    '''
-    county_data = load_csv(population_file, ['County','Latitude', 'Longitude'])
-    distribution_data = load_csv(distribution_center_file, ['Name','Latitude', 'Longitude'])
-    
+
+    county_data = load_csv(
+        population_file, ['County', 'Latitude', 'Longitude'])
+    distribution_data = load_csv(distribution_center_file, [
+                                 'Name', 'Latitude', 'Longitude'])
+
     return distribution_data, county_data
-
-def add_coords(df):
-    '''
-    Adds new column, result of merging Latitude and Longitude columns into one column called coordinates
-    '''
-    coords_col = []
-    for _, row in df.iterrows():
-        coords = (row['Latitude'], row['Longitude'])
-        coords_col.append(coords)
-    df['coordinates'] = coords_col
-    
-    return df
 
 
 def find_closest_center(distribution_data, county_data):
     '''
     Ranks the closest vaccine distribution center to a zip code by distance
-    
-    Returns a np array with index i as zipcode i. array[i] is an array of tuples (center j, dist to center j from zipcode i)
+
+        PARAM:
+            distribution_data (dataframe) - maps dist sites to their coords
+            county_data (dataframe) - maps counties to their coords
+
+        RETURNS:
+            np array with index i as zipcode i. array[i] is an array of tuples (center j, dist to center j from zipcode i)
     '''
     county_to_closest_center = np.empty(len(county_data))
-    
+
     for county_i, county_row in county_data.iterrows():
         # get county data
-        county, num, lat, lon = county_row['County [2]'], county_row['Number 65+ Population'], county_row['Latitude'], county_row['Longitude']
+        county, num, lat, lon = county_row['County [2]'], county_row[
+            'Number 65+ Population'], county_row['Latitude'], county_row['Longitude']
         ranked = []
         for _, d_row in distribution_data.iterrows():
             d_lat, d_lon = d_row['Latitude'], d_row['Longitude']
             dist = great_circle_distance((lat, lon), (d_lat, d_lon))
             ranked.append((d_i, dist))
-        ranked.sort(key=lambda x: x[1]) # sort by centers by distance
+        ranked.sort(key=lambda x: x[1])  # sort by centers by distance
         ranked = np.array(ranked)
-        county_to_closest_center[county_i] = ranked # save this zipcode's closest centers
-    
+        # save this zipcode's closest centers
+        county_to_closest_center[county_i] = ranked
+
     return county_to_closest_center
 
 
-def build_bqm(potential_new_cs_nodes, num_poi, pois, num_cs, charging_stations, num_new_cs):
-    """ Build bqm that models our problem scenario for the hybrid sampler. """
+def build_bqm(potential_new_dist_nodes, num_poi, pois, num_dist, dist_sites, num_new_dist):
+    """
+    Build bqm that models our problem scenario for the hybrid sampler.
+
+        PARAMS:
+            potential_new_dist_nodes (list) - coordinates of potential distribution sites to choose from
+            num_poi (int) - number of counties
+            pois (list) - coordinates of counties
+            num_dist (int) - number of current distribution sites
+            dist_sites (list) - coordinates of current distribution sites
+            num_new_dist (int) - desired number of distribution sites to add
+
+        RETURNS:
+            bqm with optimization parameters set
+    """
 
     # Tunable parameters
-    gamma1 = len(potential_new_cs_nodes) * 4
-    gamma2 = len(potential_new_cs_nodes) / 3
-    gamma4 = len(potential_new_cs_nodes) ** 3
+    gamma1 = len(potential_new_dist_nodes) * 4
+    gamma2 = len(potential_new_dist_nodes) / 3
+    gamma4 = len(potential_new_dist_nodes) ** 3
 
-    # Build BQM using adjVectors to find best new charging location s.t. min 
-    # distance to POIs and max distance to existing charging locations
-    bqm = dimod.AdjVectorBQM(len(potential_new_cs_nodes), 'BINARY')
+    # Build BQM using adjVectors to find best new distribution location s.t. min
+    # distance to POIs and max distance to existing distribution locations
+    bqm = dimod.AdjVectorBQM(len(potential_new_dist_nodes), 'BINARY')
 
     # Constraint 1: Min average distance to POIs
     if num_poi > 0:
-        for i in range(len(potential_new_cs_nodes)):
+        for i in range(len(potential_new_dist_nodes)):
             # Compute average distance to POIs from this node
             avg_dist = 0
-            cand_loc = potential_new_cs_nodes[i]
+            cand_loc = potential_new_dist_nodes[i]
             for loc in pois:
-                dist = (cand_loc[0]**2 - 2*cand_loc[0]*loc[0] + loc[0]**2 
-                                    + cand_loc[1]**2 - 2*cand_loc[1]*loc[1] + loc[1]**2)
-                avg_dist += dist / num_poi 
+                dist = (cand_loc[0]**2 - 2*cand_loc[0]*loc[0] + loc[0]**2
+                        + cand_loc[1]**2 - 2*cand_loc[1]*loc[1] + loc[1]**2)
+                avg_dist += dist / num_poi
             bqm.linear[i] += avg_dist * gamma1
 
-    # Constraint 2: Max distance to existing chargers
-    if num_cs > 0:
-        for i in range(len(potential_new_cs_nodes)):
+    # Constraint 2: Max distance to existing distribution sites
+    if num_dist > 0:
+        for i in range(len(potential_new_dist_nodes)):
             # Compute average distance to POIs from this node
             avg_dist = 0
-            cand_loc = potential_new_cs_nodes[i]
-            for loc in charging_stations:
+            cand_loc = potential_new_dist_nodes[i]
+            for loc in dist_sites:
                 dist = (-1*cand_loc[0]**2 + 2*cand_loc[0]*loc[0] - loc[0]**2
-                                    - cand_loc[1]**2 + 2*cand_loc[1]*loc[1] - loc[1]**2)
-                avg_dist += dist / num_cs
+                        - cand_loc[1]**2 + 2*cand_loc[1]*loc[1] - loc[1]**2)
+                avg_dist += dist / num_dist
             bqm.linear[i] += avg_dist * gamma2
 
-    # Constraint 4: Choose exactly num_new_cs new charging locations
-    bqm.update(dimod.generators.combinations(bqm.variables, num_new_cs, strength=gamma4))
+    # Constraint 3: Choose exactly num_new_dist new distribution locations
+    bqm.update(dimod.generators.combinations(
+        bqm.variables, num_new_dist, strength=gamma4))
 
     return bqm
 
 
-
-def run_bqm(bqm, sampler, **kwargs):
+def build_graph(population_nodes, distribution_nodes):
     """ 
-    Solve the bqm with the provided sampler
+    Creates the graph of Texas using counties and distribution centers
+    as nodes.
+
+        PARAMS:
+            population_nodes (list) - coordinates of the counties stored as tuples
+            distribution_nodes (list) - coordinates of the distribution centers stored as tuples
+
+        RETURNS:
+            Graph G containing all counties and dist centers as nodes with distance
+            as edge weights, list of coord of dist sites, and list of cooord of counties
     """
 
-    sampleset = sampler.sample(bqm, **kwargs)
-
-    ss = sampleset.first.sample
-    new_charging_nodes = [potential_new_cs_nodes[k] for k, v in ss.items() if v == 1]
-
-    return new_charging_nodes
-
-def build_graph(population_nodes, distribution_nodes):
     G = nx.Graph()
     sites = []
     counties = []
@@ -144,9 +167,9 @@ def build_graph(population_nodes, distribution_nodes):
         loc1 = (lat1, lon1)
         sites.append(loc1)
         sites.append(loc1)
-        
+
         G.add_node(name1)
-        
+
         for j in range(i, len(population_nodes)):
             # POIS
             name2 = population_nodes["County"][j]
@@ -154,57 +177,81 @@ def build_graph(population_nodes, distribution_nodes):
             lon2 = float(population_nodes["Longitude"][j])
             population = float(population_nodes['Number 65+ Population'][j])
             G.add_node(name2)
-            
+
             loc2 = (lat2, lon2)
             weight = great_circle_distance(loc1, loc2) * population
             counties.append(loc2)
-            
-            
+
             G.add_edge(name1, name2, weight=weight)
-            
-    
+
     return G, sites, counties
 
-def printout_solution_to_cmdline(pois, num_poi, charging_stations, num_cs, new_charging_nodes, num_new_cs):
-    """ Print solution statistics to command line. """
+
+def printout_solution_to_cmdline(pois, num_poi, dist_sites, num_dist, new_dist_nodes, num_new_dist):
+    """ 
+    Print solution statistics to command line. 
+
+        PARAMS:
+            pois (list) - coordinates of counties
+            num_poi (int) - number of counties
+            dist_sites (list) - coordinates of current distribution sites
+            num_dist (int) - number of current distribution sites
+            new_dist_nodes (list) - coordinates of potential distribution sites to choose from
+            num_new_dist (int) - desired number of distribution sites to add    
+    """
 
     print("\nSolution returned: \n------------------")
-    
-    print("\nNew charging locations:\t\t\t\t", new_charging_nodes)
+
+    print("\nNew distribution locations:\t\t\t\t", new_dist_nodes)
 
     if num_poi > 0:
-        poi_avg_dist = [0] * len(new_charging_nodes)
+        poi_avg_dist = [0] * len(new_dist_nodes)
         for loc in pois:
-            for i, new in enumerate(new_charging_nodes):
-                poi_avg_dist[i] += sum(abs(a - b) for a, b in zip(new, loc)) / num_poi
+            for i, new in enumerate(new_dist_nodes):
+                poi_avg_dist[i] += sum(abs(a - b)
+                                       for a, b in zip(new, loc)) / num_poi
         print("Average distance to POIs:\t\t\t", poi_avg_dist)
 
-    if num_cs > 0:
-        old_cs_avg_dist = [sum(abs(a - b) for a, b in zip(new, loc) for loc in charging_stations) / num_cs for new in new_charging_nodes]
-        print("Average distance to old charging stations:\t", old_cs_avg_dist)
+    if num_dist > 0:
+        old_cs_avg_dist = [sum(abs(a - b) for a, b in zip(new, loc)
+                               for loc in dist_sites) / num_dist for new in new_dist_nodes]
+        print("Average distance to old distribution sites:\t", old_cs_avg_dist)
 
-    if num_new_cs > 1:
+    if num_new_dist > 1:
         new_cs_dist = 0
-        for i in range(num_new_cs):
-            for j in range(i+1, num_new_cs):
-                new_cs_dist += abs(new_charging_nodes[i][0]-new_charging_nodes[j][0])+abs(new_charging_nodes[i][1]-new_charging_nodes[j][1])
-        print("Distance between new chargers:\t\t\t", new_cs_dist)
-        
-def run_bqm_and_collect_solutions(bqm, sampler, potential_new_cs_nodes, **kwargs):
-    """ Solve the bqm with the provided sampler to find new charger locations. """
+        for i in range(num_new_dist):
+            for j in range(i+1, num_new_dist):
+                new_cs_dist += abs(new_dist_nodes[i][0]-new_dist_nodes[j][0])+abs(
+                    new_dist_nodes[i][1]-new_dist_nodes[j][1])
+        print("Distance between new distribution sites:\t\t\t", new_cs_dist)
+
+
+def run_bqm_and_collect_solutions(bqm, sampler, potential_new_dist_nodes, **kwargs):
+    """ Solve the bqm with the provided sampler to find new distribution locations. """
 
     sampleset = sampler.sample(bqm, **kwargs)
 
     ss = sampleset.first.sample
-    new_charging_nodes = [potential_new_cs_nodes[k] for k, v in ss.items() if v == 1]
+    new_dist_nodes = [potential_new_dist_nodes[k]
+                          for k, v in ss.items() if v == 1]
 
-    return new_charging_nodes
-    
-    
+    return new_dist_nodes
+
+
 def iterate_bqm_and_graph(iters, run_num):
+    """
+    Solve the BQM and generate graphs of the new distribution sites in the
+    results folder. 
+
+        PARAMS:
+            iters (int) - number of times to repeat calculation
+            run_num (int) - folder name
+    """
+
     step = 77 // iters
-    d, c = load_data('data/Distribution_center_locations_TX.csv', 'data/TX_Counties.csv')
-    
+    d, c = load_data('data/Distribution_center_locations_TX.csv',
+                     'data/TX_Counties.csv')
+
     os.mkdir("results/run_{}".format(run_num))
 
     for i in range(0, 77, step):
@@ -218,15 +265,15 @@ def iterate_bqm_and_graph(iters, run_num):
         sampler = LeapHybridSampler()
         print("\nRunning scenario on", sampler.solver.id, "solver...")
 
-        new_charging_nodes = run_bqm_and_collect_solutions(bqm, sampler, sites)
+        new_dist_nodes = run_bqm_and_collect_solutions(bqm, sampler, sites)
 
         # Print results to commnand-line for user
-        poi_avg_dist = printout_solution_to_cmdline(counties, len(counties), [], 0, new_charging_nodes,
-                                                    len(new_charging_nodes))
+        poi_avg_dist = printout_solution_to_cmdline(counties, len(counties), [], 0, new_dist_nodes,
+                                                    len(new_dist_nodes))
 
         try:
-            latitude_list = [x[0] for x in new_charging_nodes]
-            longitude_list = [x[1] for x in new_charging_nodes]
+            latitude_list = [x[0] for x in new_dist_nodes]
+            longitude_list = [x[1] for x in new_dist_nodes]
 
             gmap = gmplot.GoogleMapPlotter(latitude_list[0],
                                            longitude_list[0], 1000)
@@ -238,56 +285,20 @@ def iterate_bqm_and_graph(iters, run_num):
             gmap.scatter(latitude_list, longitude_list, 'blue',
                          size=8000, marker=False)
 
-#             gmap.draw("map_{}.html".format(run_num, i))
-            
+            gmap.draw("results/run_{}/map_{}.html".format(run_num, i))
         except:
             print("STEP 0")
 
-        print("charging_nodes: {}".format(new_charging_nodes))
+        print("distribution_nodes: {}".format(new_dist_nodes))
         print("poi_distance: {}".format(poi_avg_dist))
-        
-        
 
-    
+        try:
+            np.savetxt("results/sites_{}.txt".format(i), new_dist_nodes)
+            np.savetxt("results/distances_{}.txt".format(i), poi_avg_dist)
+        except:
+            print("results not saved")
+
+
 if __name__ == '__main__':
-    d, c = load_data('data/Distribution_center_locations_TX.csv', 'data/TX_Counties.csv')
-#     d = add_coords(d)
-#     c = add_coords(c)
-    
-    # Build large grid graph for city
-    G, sites, counties = build_graph(c, d)
-    
-    # Build BQM
-    n = 70
-    bqm = build_bqm(sites, len(sites), counties, 0, [], n)
 
-    # Run BQM on HSS
-    sampler = LeapHybridSampler()
-    print("\nRunning scenario on", sampler.solver.id, "solver...")
-    
-    new_charging_nodes = run_bqm_and_collect_solutions(bqm, sampler, sites)
-
-    # Print results to commnand-line for user
-    printout_solution_to_cmdline(sites, len(sites), [], 0, new_charging_nodes, len(new_charging_nodes))
-    
-    latitude_list = [x[0] for x in new_charging_nodes]
-    longitude_list = [x[1] for x in new_charging_nodes]
-
-    gmap = gmplot.GoogleMapPlotter(latitude_list[0],
-                                   longitude_list[0], 10000)
-
-    gmap.apikey = "AIzaSyB9N6G3mW559tfaPnaI_QVJo5MaTiwtOkE"
-
-    # scatter method of map object
-    # scatter points on the google map
-    gmap.scatter(latitude_list, longitude_list, 'blue',
-                 size=8000, marker=False)
-    
-    gmap.draw("map_{}.html".format(n))
-    
-    
-
-    # Create scenario output image
-#     save_output_image(G, pois, charging_stations, new_charging_nodes)
-
-    
+    iterate_bqm_and_graph(10, 13)
